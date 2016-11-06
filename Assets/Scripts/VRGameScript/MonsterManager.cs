e697c96c887c8f65a72366701f410853d3a21179
Jaewoo Kim <rhdqud7296@naver.com>
2016-11-07 01:18:06 +0900

    VR

--- Assets/Scripts/VRGameScript/MonsterManager.cs
+++ Assets/Scripts/VRGameScript/MonsterManager.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MonsterManager : MonoBehaviour
 {
     public Transform[] points;
     public GameObject monsterPrefab;
+    public GameObject myCamera;
+    public List<GameObject> monsterList;
 
     public float createTime = 2.0f;
-    public int maxMonster = 3;
+    public int maxMonster = 9;
     public bool isGameOver = false;
 
     void Start()
     {
+        monsterList = new List<GameObject>();
         points = GameObject.Find("SpawnPoint").GetComponentsInChildren<Transform>();
 
         if(points.Length > 0)
@@ -22,6 +26,8 @@ public class MonsterManager : MonoBehaviour
 	
     IEnumerator CreateMonster()
     {
+        GameObject monster;
+
         while (!isGameOver)
         {
             int monsterCount = (int)GameObject.FindGameObjectsWithTag("MONSTER").Length;
@@ -30,9 +36,17 @@ public class MonsterManager : MonoBehaviour
             {
                 yield return new WaitForSeconds(createTime);
 
+                //Vector3 v;
                 int idx = Random.Range(1, points.Length);
+                
+                monster = (GameObject)Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+                monsterList.Add(monster);
 
-                Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+                monster.transform.LookAt(myCamera.transform.position);
+                monster.transform.Rotate(-90, 0, 0);
+                //v = monster.transform.position;
+                //v.y = 187;
+                //monster.transform.position = v;
             }
             else
             {
